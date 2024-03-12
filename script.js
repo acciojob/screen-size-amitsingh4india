ebd3976b77da887bd380dbc4dfa3605cda07c3ba
amitsingh4india <76247705+amitsingh4india@users.noreply.github.com>
2024-03-12 21:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+w=window.innerWidth;
+h=window.innerHeight;
+
+
+function updateSize(){
+	let w=window.innerWidth;
+	let h=window.innerHeight;
+	document.getElementById('size').innerText="Width: "+w+" and "+h
+}
+updateSize()
